27f3af2336379a6fca1f258bb85768e935f8a129
Alex T <alextai@pacificamerican.org>
2016-09-01 07:06:04 +0800

    Delete SolarSystemDir.py

--- SolarSystemDir.py
+++
@@ -1,8 +0,0 @@
-‘’’This program shows the size and distance of the planets in the solar system with to the Earth.
-User enters the planet name.
-‘’’
-
-planet = input(‘Enter the planet name(Venus, Mars, Jupiter, Neptune, Saturn, Mercury)’)
-
-if planet != ‘Venus’ + ’Mars’ + ‘Jupiter’ + ’Neptune’ + ‘Saturn’ + ‘Mercury’:
-	 print(‘That is not a planet’)
