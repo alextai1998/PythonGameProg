f0be144226ab896db53f5edb19641e98d0ef13f3
Alex Tai <alexjasontai@gmail.com>
2016-09-01 09:31:10 +0800

    random backup

---
+++ Import Module Excercise.py
@@ -0,0 +1,10 @@
+import sys
+
+print("The arguments passed were: ")
+print (sys.argv)
+
+a = float(sys.argv[1]) # second argument
+b = float(sys.argv[2]) # second argument
+
+print("The average is ")
+print((a + b) / 2)
