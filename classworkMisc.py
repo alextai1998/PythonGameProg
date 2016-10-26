31298e3bbb0303a7d46edf31cfa7d61679774f8a
Alex Tai <alexjasontai@gmail.com>
2016-10-26 09:37:30 +0800

    journal entry on dictionary + errors of other programmers

--- classworkMisc.py
+++ classworkMisc.py
@@ -19,6 +19,46 @@ b = [x for x in a if x % 2 == 0]
 print(b)
 """
 
+"""
 import alexLib as al
 
 al.findNextWord("i", al.readTxt("lyrics.txt"))
+"""
+
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = [.5*x-10 for x in range(200)]
+points = [(x, y) for x in t for y in t if np.sqrt(x**2+y**2) < 10]
+for x, y in points:
+    plt.scatter(x, y, marker="x", c="turquoise")
+
+plt.show()
+"""
+
+"""
+import os
+
+systemFiles = [filename for filename in os.listdir("/Users/alextai/Desktop") if filename.startswith("~")]
+print(systemFiles)
+"""
+
+"""
+mapping = {1: 22.5, 8: 13.4, 10: 12.1}
+hello = [v for k, v in mapping.items()]
+acc = 0
+for v in hello:
+    acc += v
+print(acc)
+print(len(mapping))
+
+print(acc / (len(mapping)))
+"""
+
+"""
+b = {"video": 0, "music": 23}
+k = list(b.keys())
+
+print(k[0])
+"""
