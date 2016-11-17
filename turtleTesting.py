56dcba05273afd93e6c319e966adee7ee08a90ed
Alex Tai <alexjasontai@gmail.com>
2016-11-17 09:47:03 +0800

    turtle tree problem + journal

--- turtleTesting.py
+++ turtleTesting.py
@@ -30,37 +30,23 @@ def drawSquare(sqrlen, turtle):
 
 # drawSquare(100, at)
 
+
+def drawY(branch, len, tur):
+    if branch == 1:
+        return
+    else:
+        tur.fd(len)
+        tur.right(20)
+        drawY(branch-1, len-10, tur)
+        tur.left(40)
+        drawY(branch-1, len-10, tur)
+        tur.right(20)
+        tur.back(len)
+
 at.left(90)
 at.fd(100)
-at.left(45)
-at.fd(50)
-at.back(50)
-at.right(90)
-at.fd(50)
-at.fd(50)
-at.left(45)
-at.fd(50)
-at.back(50)
-at.right(90)
-at.fd(50)
-at.fd(50)
-at.left(45)
-at.fd(50)
-at.back(50)
-at.right(90)
-at.fd(50)
-at.fd(50)
-at.left(45)
-at.fd(50)
-at.back(50)
-at.right(90)
-at.fd(50)
-at.fd(50)
-at.left(45)
-at.fd(50)
-at.back(50)
-at.right(90)
-at.fd(50)
+drawY(7, 80, at)
+
 
 # Close window with a click on the screen
 myWin.exitonclick()
