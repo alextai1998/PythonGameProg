81876d004b778c293de3c1bb49e121d8c8f27abf
Alex Tai <alexjasontai@gmail.com>
2016-12-15 14:33:46 +0800

    codingame journal

--- classworkMisc.py
+++ classworkMisc.py
@@ -15,3 +15,16 @@
 # printPicnic(picnic, 12, 5)
 # printPicnic(picnic, 20, 6)
 
+
+
+newList = []
+heights = [3,2,6,2,4,6,3]
+hello = list(enumerate(heights))
+print(hello)
+newList = [(v, k) for k,v in hello]
+print(newList)
+newList.sort()
+print(newList)
+
+shootList = [v for k,v in newList]
+print(shootList)
