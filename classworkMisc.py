095f4f48aa01336433c641bff52f0a861fac71f0
Alex Tai <alexjasontai@gmail.com>
2016-12-21 12:01:24 +0800

    prep session xmas

--- classworkMisc.py
+++ classworkMisc.py
@@ -17,14 +17,44 @@
 
 
 
-newList = []
-heights = [3,2,6,2,4,6,3]
-hello = list(enumerate(heights))
-print(hello)
-newList = [(v, k) for k,v in hello]
-print(newList)
-newList.sort()
-print(newList)
+# newList = []
+# heights = [3,2,6,2,4,6,3]
+# hello = list(enumerate(heights))
+# print(hello)
+# newList = [(v, k) for k,v in hello]
+# print(newList)
+# newList.sort()
+# print(newList)
+#
+# shootList = [v for k,v in newList]
+# print(shootList)
 
-shootList = [v for k,v in newList]
-print(shootList)
+
+
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+
+# Write an action using print
+# To debug: print("Debug messages...", file=sys.stderr)
+n = 6
+temps = ['42', '-5', '12', '21', '5', '24']
+smallestTemp = 5526
+
+
+if n == 0:
+    print(0)
+else:
+    for i in temps:
+        if abs(int(i)) == abs(smallestTemp):
+            smallestTemp = abs(int(i))
+        elif abs(int(i)) < abs(smallestTemp):
+            smallestTemp = int(i)
+
+print(temps)
+
+print(smallestTemp)
