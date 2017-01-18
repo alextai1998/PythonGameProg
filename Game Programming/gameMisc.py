7d6d560b2e3209a3a839da7587392cd8b5262452
Alex Tai <alexjasontai@gmail.com>
2017-01-18 09:48:32 +0800

    journal entry on pygame import

---
+++ Game Programming/gameMisc.py
@@ -0,0 +1,9 @@
+# Import the pygame library and initialise the game engine
+import pygame
+pygame.init()
+
+
+# Open a new window
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("My First Game")
