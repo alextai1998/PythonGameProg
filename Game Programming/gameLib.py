5c83bc49359fb936f1d5f9c536a94aabb6a88582
Alex Tai <alexjasontai@gmail.com>
2017-02-19 23:27:04 +0800

    snake game

--- Game Programming/gameLib.py
+++ Game Programming/gameLib.py
@@ -1,16 +1,25 @@
-"""
-This program serves as a library for all of the functions Alex wrote for Game Programming.
-"""
+# """
+# This program serves as a library for all of the functions Alex wrote for Game Programming.
+# """
+#
+# import sys, tty, termios
+#
+#
+# def getkey():
+#     fd = sys.stdin.fileno()
+#     old_setting = termios.tcgetattr(fd)
+#     try:
+#         tty.setraw(sys.stdin.fileno())
+#         ch = sys.stdin.read(1)
+#     finally:
+#         termios.tcsetattr(fd, termios.TCSADRAIN, old_setting)
+#     return ch
 
-import sys, tty, termios
+
+dict = {'x': 5, 'y': 5}
+
+if {'x': 5, 'y': 5} in list:
+    print("{'x': 5, 'y': 5} is in list!")
 
 
-def getkey():
-    fd = sys.stdin.fileno()
-    old_setting = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_setting)
-    return ch
+print(list[0])
