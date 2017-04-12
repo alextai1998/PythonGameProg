6abe4f597173dc1455bd3db8208a5bc5c32b6b16
Alex Tai <alexjasontai@gmail.com>
2017-04-12 09:30:08 +0800

    finalized demo for checkbox

--- GUI/checkboxDemo.py
+++ GUI/checkboxDemo.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QTextEdit, QSpinBox, QCheckBox
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QCheckBox
+from PyQt5.QtGui import QIcon
 
 
 class Example(QWidget):
@@ -13,14 +14,40 @@ class Example(QWidget):
 
         # --- add a checkbox
         self.check = QCheckBox('This is a checkbox!', self)
+        self.check.setTristate(True)  # Allows for tristate!
         self.check.resize(250, 30)
-        self.check.move(100, 90)
+        self.check.move(100, 30)
         self.check.stateChanged.connect(self.dosth)
+        self.check.setIcon(QIcon("/Users/alextai/Desktop/PythonGameProg/GUI/emoji_alien_facebook.png"))
+
+
+        # --- add a push button
+        self.btn1 = QPushButton('Reset', self)
+        self.btn1.setToolTip('This button resets the checkboxes!')
+        self.btn1.resize(250, 90)
+        self.btn1.move(55, 100)
+        self.btn1.clicked.connect(self.reset)
 
         self.show()
 
     def dosth(self):
-        self.setWindowTitle('The checkbox was checked!')
+        # if self.check.isChecked():
+        #     self.setWindowTitle('The checkbox was checked!')
+        # else:
+        #     self.setWindowTitle('The checkbox was unchecked!')
+
+        state = self.check.checkState()
+
+        if state == 0:
+            self.setWindowTitle('The checkbox is unchecked!')
+        elif state == 2:
+            self.setWindowTitle('The checkbox is checked!')
+        else:
+            self.setWindowTitle('The checkbox is unchanged!')
+
+    def reset(self):
+        self.check.setChecked(False)
+        self.setWindowTitle('CheckBox Demonstration')
 
 
 # --- main program
